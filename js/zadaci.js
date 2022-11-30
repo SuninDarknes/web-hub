e153f38954931e23763fc3011d55d5c4e1e450b3
SuninDarknes <sunindark00@gmail.com>
2022-11-30 20:26:16 +0100

    update

--- js/zadaci.js
+++ js/zadaci.js
@@ -1,11 +1,14 @@
-document.querySelector(".task-container").addEventListener("click", (e) =>{
-  elem = e.target;
-  for(let i =0;i<10;i++){
-    if(elem.classList.contains("task-container"))break;
-    if(elem.classList.contains("html")) return;
-    elem = elem.parentElement;
-  }
-  elem.classList.add("show");
-  hiddenElem = elem.querySelector(".hidden");
-  elem.style.height = (50+hiddenElem.clientHeight) +"px"
+containers = document.querySelectorAll(".task-container")
+containers.forEach(element => {
+  element.addEventListener("click", (e) =>{
+    elem = e.target;
+    for(let i =0;i<10;i++){
+      if(elem.classList.contains("task-container"))break;
+      if(elem.classList.contains("html")) return;
+      elem = elem.parentElement;
+    }
+    elem.classList.add("show");
+    hiddenElem = elem.querySelector(".hidden");
+    elem.style.height = (50+hiddenElem.clientHeight) +"px"
+  });
 });
