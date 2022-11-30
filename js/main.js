6ab4bf4f484fc4a93e05d8e449b338709a09edf0
SuninDarknes <sunindark00@gmail.com>
2022-11-30 22:04:30 +0100

    update

--- js/main.js
+++ js/main.js
@@ -12,20 +12,22 @@ function switchMode() {
       $(':root').css('--background', '#02101f');
       $(':root').css('--text-color', '#FFF');
       $(':root').css('--shadows', 'rgba(0, 0, 0, 1)');
-      $(':root').css('--neon', '#5CDB85');
+      $(':root').css('--neon', '#93EEDF');
       $("[for=mode]").html('<img src="svg/light_mode.svg">');
 
-      $(':root').css('--task-background', '#071827');
+      $(':root').css('--task-background', '#121e2c');
       $(':root').css('--light-shadow', 'rgba(0, 0, 0, 0.5)');
+      $(':root').css('--focused-background', '#18313d');
   } else {
       $(':root').css('--background', '#FFF');
       $(':root').css('--text-color', '##02101f');
       $(':root').css('--shadows', 'rgba(0, 0, 0, .15)');
-      $(':root').css('--neon', '#39FF14');
+      $(':root').css('--neon', '#93EEDF');
       $("[for=mode]").html('<img src="svg/dark_mode.svg">');
 
       $(':root').css('--task-background', '#FFF');
       $(':root').css('--light-shadow', 'rgba(0, 0, 0, 0.25)');
+      $(':root').css('--focused-background', 'rgba(	240, 250, 255, .95)');
 
   }
 }
